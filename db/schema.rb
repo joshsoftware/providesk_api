e3d3e9cb1f2d19e7bed02a7bff340c606396d09a
rutujanirmal <rutujanirmal8888@gmail.com>
2022-09-17 00:57:27 +0530

    Generate Ticket Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_16_165434) do
+ActiveRecord::Schema.define(version: 2022_09_16_181534) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "priority", default: 0
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "departments", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "organizations", force: :cascade do |t|
     t.string "name", null: false
@@ -28,17 +41,41 @@ ActiveRecord::Schema.define(version: 2022_09_16_165434) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "tickets", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "ticket_number"
+    t.integer "status"
+    t.integer "priority"
+    t.integer "type"
+    t.datetime "resolved_at"
+    t.bigint "resolver_id"
+    t.bigint "requester_id"
+    t.bigint "department_id"
+    t.bigint "category_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_tickets_on_category_id"
+    t.index ["department_id"], name: "index_tickets_on_department_id"
+    t.index ["requester_id"], name: "index_tickets_on_requester_id"
+    t.index ["resolver_id"], name: "index_tickets_on_resolver_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "roles_id"
-    t.bigint "organizations_id"
-    t.index ["organizations_id"], name: "index_users_on_organizations_id"
-    t.index ["roles_id"], name: "index_users_on_roles_id"
+    t.bigint "role_id"
+    t.bigint "organization_id"
+    t.index ["organization_id"], name: "index_users_on_organization_id"
+    t.index ["role_id"], name: "index_users_on_role_id"
   end
 
-  add_foreign_key "users", "organizations", column: "organizations_id"
-  add_foreign_key "users", "roles", column: "roles_id"
+  add_foreign_key "tickets", "categories"
+  add_foreign_key "tickets", "departments"
+  add_foreign_key "tickets", "users", column: "requester_id"
+  add_foreign_key "tickets", "users", column: "resolver_id"
+  add_foreign_key "users", "organizations"
+  add_foreign_key "users", "roles"
 end
