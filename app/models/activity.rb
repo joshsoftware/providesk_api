da02df573dd31d63b6acd508b6657bd587452725
Nandini Jhanwar <nandinijhanwar67@gmail.com>
2022-09-17 02:12:25 +0530

    Add ticket in activites model as foriegn key and assign associations

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,5 @@
 class Activity < ApplicationRecord
+  belongs_to :ticket
 
   enum current_status: {
     "assigned": 0,
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_16_202136) do
+ActiveRecord::Schema.define(version: 2022_09_16_203859) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2022_09_16_202136) do
     t.integer "current_ticket_status", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "ticket_id"
+    t.index ["ticket_id"], name: "index_activities_on_ticket_id"
   end
 
   create_table "categories", force: :cascade do |t|
