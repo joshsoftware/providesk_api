137008a3c0766297414ce7603b32dae5608d8876
Nandini Jhanwar <nandinijhanwar67@gmail.com>
2022-09-16 20:22:54 +0530

    Generate migration for Role having unique field 'name'

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_16_134637) do
+ActiveRecord::Schema.define(version: 2022_09_16_143643) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 2022_09_16_134637) do
   create_table "organizations", force: :cascade do |t|
     t.string "name", null: false
     t.string "domain", default: [], array: true
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
