9fc4f1b425a03666363597401ecb7f1001ee3564
Vishwajeetsingh Desurkar <vishwajeetsinghd@gmail.com>
2022-09-17 00:28:42 +0530

    Add department reference in users with option true for role, organization and department

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  belongs_to :organization
-  belongs_to :role
-  belongs_to :department
+  belongs_to :organization, optional: true
+  belongs_to :role, optional: true
+  belongs_to :department, optional: true
 
   validates :email, uniqueness: true
 end
