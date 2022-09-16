dcc1ba6d141052f870bb85a032ecd0d9458bcc69
Vishwajeetsingh Desurkar <vishwajeetsinghd@gmail.com>
2022-09-17 00:31:38 +0530

    Change roles to be compulsory in users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   belongs_to :organization, optional: true
-  belongs_to :role, optional: true
+  belongs_to :role
   belongs_to :department, optional: true
 
   validates :email, uniqueness: true
