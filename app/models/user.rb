413123cf7203e81d296d0dc6d178b20b716a0696
rutujanirmal <rutujanirmal8888@gmail.com>
2022-09-17 05:26:38 +0530

    Add uniqueness validation to email

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   has_many :tickets
 
   validates :name, presence: true
-  validates :email,presence: true
+  validates :email,presence: true, uniqueness: true
   validates_assocaited :tickets
 
 end
