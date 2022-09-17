fc7656715c347fc42cf06e4375a797d7553e051d
Nandini Jhanwar <56627312+Nandini2806@users.noreply.github.com>
2022-09-17 16:47:37 +0530

    API for creating categories (#22)
    * Write API to create categories along with test cases
    
    * Resolved the changes for mentioning status and handling hash of boolean values for existing category name suggested in PR
    
    * Remove byebug from the categories_controller and create action
    
    Co-authored-by: Vishwajeetsingh Desurkar <32262119+Selectus2@users.noreply.github.com>

---
+++ app/controllers/api/v1/categories_controller.rb
@@ -0,0 +1,18 @@
+module Api::V1
+  class CategoriesController < ApplicationController
+    def create
+      result = Categories::V1::Create.new(categories_params, current_user).call
+      if result[:status]
+        render json: { message: I18n.t('categories.success.create') }
+      else
+        render json: { message: I18n.t('categories.error.create') }, status: :unprocessable_entity
+      end
+    end
+
+    private 
+
+    def categories_params
+      params.require(:categories).permit(:name, :priority, :department_id)    
+    end
+  end
+end
