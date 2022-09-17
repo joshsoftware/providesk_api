6da03fbb08d5be3bdf35becb7e1f481be3209a50
Akansha <akanshak984@gmail.com>
2022-09-17 08:30:36 +0530

    Get secret key value based on enviornment

--- app/services/json_web_token.rb
+++ app/services/json_web_token.rb
@@ -4,13 +4,13 @@ class JsonWebToken
   class << self
     def encode(payload, exp = 24.hours.from_now)
       payload[:exp] = exp.to_i
-      JWT.encode(payload, Rails.application.secrets.secret_key_base)
+      JWT.encode(payload, secret_key_base)
     rescue StandardError
       nil
     end
 
     def decode(token)
-      body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
+      body = JWT.decode(token, secret_key_base)[0]
       HashWithIndifferentAccess.new body
     rescue StandardError
       nil
@@ -26,6 +26,11 @@ class JsonWebToken
 
     def expired(payload)
       Time.zone.at(payload[:exp]) < Time.zone.now
+    end
+
+    def secret_key_base
+      Rails.application.secret_key_base if Rails.env.eql?('production')
+      Rails.application.secrets.secret_key_base
     end
   end
 end
