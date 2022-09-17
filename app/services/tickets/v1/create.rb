8278a83805b9555e883512a5736a593d8c8f1dfd
Shubham Kumar <shubham01384@gmail.com>
2022-09-17 11:01:50 +0530

    Changes done according to comments

--- app/services/tickets/v1/create.rb
+++ app/services/tickets/v1/create.rb
@@ -9,15 +9,15 @@ module Tickets::V1
       @description = params[:description]
       @category_id = params[:category_id]
       @department_id = params[:department_id]
-      @resolver_id = params[:resolver_id]
       @ticket_type = params[:ticket_type]
     end
 
     def call
-      set_values && save_ticket
+      set_priority_and_requester_id_and_resolver_id && save_ticket
     end
 
-    def set_values
+    def set_priority_and_requester_id_and_resolver_id
+      @resolver_id = User.find_by(department_id: department_id)
       @priority = Category.find_by(id: category_id).priority
       @requester_id = current_user.id
     end
