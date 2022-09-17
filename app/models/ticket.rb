b495705b7117910f3d9d4c0efbc915333549e1b4
rutujanirmal <rutujanirmal8888@gmail.com>
2022-09-17 05:45:38 +0530

    Rearrange associations and validations

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,7 +1,14 @@
 class Ticket < ApplicationRecord
   has_many :activities
-  
+  belongs_to :resolver, :class_name => 'User', :foreign_key => 'resolver_id'
+  belongs_to :requester, :class_name => 'User', :foreign_key => 'requester_id'
+  belongs_to :department
+  belongs_to :category
+
   validates_assocaited :activities
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :ticket_type, presence: true
 
   enum status: {
     "assigned": 0,
@@ -22,13 +29,5 @@ class Ticket < ApplicationRecord
     "complaint": 0,
     "request": 1
   }
-
-  validates :title, presence: true
-  validates :description, presence: true
-  validates :ticket_type, presence: true
-
-  belongs_to :resolver, :class_name => 'User', :foreign_key => 'resolver_id'
-  belongs_to :requester, :class_name => 'User', :foreign_key => 'requester_id'
-  belongs_to :department
-  belongs_to :category
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   belongs_to :organization, optional: true
   belongs_to :role
   belongs_to :department, optional: true
-
   has_many :tickets
 
   validates :name, presence: true
