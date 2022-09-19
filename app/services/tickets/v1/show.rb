df11844f58343e72b6202571e8f0d078d0f2097c
Shubham Kumar <shubham01384@gmail.com>
2022-09-19 14:51:56 +0530

    Changes updated according to comments

--- app/services/tickets/v1/show.rb
+++ app/services/tickets/v1/show.rb
@@ -2,30 +2,30 @@
 
 module Tickets::V1
   class Show
-
     def initialize(params)
       @ticket_id = params[:id]
     end
 
     def call
-      find_ticket && response 
-		end
+      find_ticket && response
+    end
 
     def find_ticket
       @ticket = Ticket.find_by(id: @ticket_id)
-			return true if @ticket
-			false
+      return true if @ticket
+
+      false
     end
 
     def response
-			@ticket.as_json(
-					only: [:id, :status, :title, :description, :ticket_number, :ticket_type, :priority, :created_at, :resolved_at],
-					include:[{ 
-							activities: { only: [:created_at, :asset_url, :description, :id]},
-							resolver: { only: [:id, :name]}
-					}],
-					methods: [:department_name, :category_name]
-			)
-		end
+      @ticket.as_json(
+        only: %i[id status title description ticket_number ticket_type priority created_at resolved_at],
+        include: [{
+          activities: { only: %i[created_at asset_url description id] },
+          resolver: { only: %i[id name] }
+        }],
+        methods: %i[department_name category_name]
+      )
+    end
   end
 end
