a148e124f063f961cf641f919b92376f8cabe107
Addy-tea-ya-official <aditya.yadav19@pccoepune.org>
2022-09-27 18:51:07 +0530

    Add service show_categories for Departments controller and update controller accordingly

--- app/controllers/api/v1/departments_controller.rb
+++ app/controllers/api/v1/departments_controller.rb
@@ -16,29 +16,15 @@ module Api::V1
     end
 
     def show_categories
-      department = Department.where(id: params[:department_id]).last
-      render json: {message: I18n.t('department.error.invalid_department_id')}, status: :unprocessable_entity and return if department.nil?
-      if(department.organization_id.eql?(current_user.organization_id))
-        categories = Category.where(department_id: department.id)
-        categories_list = []
-        categories.each do |category|
-          categories_list.push(
-            {
-              name: category.name,
-              id: category.id
-            }
-          )     
-        end
-        render json:{
-          data: {
-            total: categories_list.length,
-            categories: categories_list
-          }
-        }
+      result = Departments::V1::ShowCategories.new(params, current_user).call
+      if result["status"]
+        render json: { data: result["data"] }
       else
-        render json:{
-          message: I18n.t('organization.error.unauthorized_user')
-        }, status: 403
+        if result["error"].eql?("No department found")
+          render json: {message: I18n.t('department.error.invalid_department_id')}, status: :unprocessable_entity
+        elsif result["error"].eql?("Unauthorized user")
+          render json:{ message: I18n.t('organization.error.unauthorized_user')}, status: 403
+        end
       end
     end
   end
---
+++ app/services/departments/v1/show_categories.rb
@@ -0,0 +1,40 @@
+module Departments::V1
+  class ShowCategories
+    def initialize(params, current_user)
+      @current_user = current_user
+      @department_id= params[:department_id]
+    end
+
+    def call
+      return show_categories if department_exists?
+      { 
+        status: false,
+        error: "No department found" 
+      }.as_json
+    end
+
+    def department_exists?
+      @department = Department.where(id: @department_id).last
+      return false if @department.nil?
+      @department
+    end
+
+    def show_categories
+      if(@department.organization_id.eql?(@current_user.organization_id))
+        categories = Category.select(:id, :name).where(department_id: @department.id)
+        { 
+          status: true,
+          data: {
+            total: categories.length,
+            categories: categories
+          }
+        }.as_json
+      else
+        { 
+          status: false,
+          error: "Unauthorized user"
+        }.as_json
+      end
+    end
+  end
+end
