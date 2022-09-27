5e2029a778d00b93ecb124a66a20612e6032c316
Pratham Goel <pratham.goel16@gmail.com>
2022-09-27 15:19:49 +0530

    Removed a reduntant test case 'when oraganization id contains data'. Also improved the messages. Also made the code more concised

--- spec/models/department_spec.rb
+++ spec/models/department_spec.rb
@@ -4,73 +4,46 @@ RSpec.describe Department, type: :model do
   let(:organization1) {Organization.create(name: 'Josh Software', domain: ['@joshsoftware.com','@joshsoftware.digital'])}
   let(:organization2) {Organization.create(name: 'Table Space', domain: ['@tablespace.com'])}
   describe 'validations' do
-    context 'when name contains data' do
+    context 'for a valid department' do
       before do
-        @department = Department.create(name: 'TAD', organization_id: organization1.id)
+        @department = Department.new(name: 'TAD', organization_id: organization1.id)
+        @same_department_diff_organization = Department.new(name: 'TAD', organization_id: organization2.id)
+        @same_oganization_diff_department = Department.new(name: 'Admin', organization_id: organization1.id)
       end
-      it 'should validate data with no errors' do
+      it 'should validate data with no errors for adding a department in a organization' do
         @department.save
         expect(@department.errors.full_messages).to be_empty
       end
-    end
-    context 'when name does not contain data' do
-      before do
-        @department = Department.create(name: '', organization_id: organization1.id)
+      it 'should validate data with no errors for adding same department in diffrent organization' do
+        @department.save
+        @same_department_diff_organization.save
+        expect(@same_department_diff_organization.errors.full_messages).to be_empty
       end
-      it 'should validate data with errors' do
+      it 'should validate data with no errors for adding diffrent department in organization' do
+        @department.save
+        @same_oganization_diff_department.save
+        expect(@same_department_diff_organization.errors.full_messages).to be_empty
+      end
+    end
+    context 'for invalid departments' do
+      before do
+        @department = Department.new(name: 'TAD', organization_id: organization1.id)
+        @same_department = Department.new(name: 'TAD', organization_id: organization1.id)        
+      end
+      it 'should validate data for presence of name' do
+        @department.name = nil
         @department.save
         expect(@department.errors.full_messages).to eq(["Name can't be blank"])
       end
-    end
-    context 'when oraganization id contains data' do
-      before do
-        @department = Department.create(name: 'TAD', organization_id: organization1.id)
-      end
-      it 'should validate data with no errors' do
-        @department.save
-        expect(@department.errors.full_messages).to be_empty
-      end
-    end
-    context 'when oraganization id does not contain data' do
-      before do
-        @department = Department.create(name: 'TAD', organization_id: nil)
-      end
-      it 'should validate data with errors' do
+      it 'should validate data for presence of organization_id' do
+        @department.organization_id = nil
         @department.save
         expect(@department.errors.full_messages).to eq(["Organization must exist", "Organization can't be blank"])
       end
-    end
-    context 'when an organization has different depertments' do
-      before do
-        @department1 = Department.create(name: 'TAD', organization_id: organization1.id)
-        @department2 = Department.create(name: 'Finance', organization_id: organization1.id)
-      end
-      it 'should validate data with no errors' do
-        @department1.save
-        @department2.save
-        expect(@department2.errors.full_messages).to be_empty
-      end
-    end
-    context 'when an organization has same depertments' do
-      before do
-        @department1 = Department.create(name: 'TAD', organization_id: organization1.id)
-        @department2 = Department.create(name: 'TAD', organization_id: organization1.id)
-      end
-      it 'should validate data with errors' do
-        @department1.save
-        @department2.save
-        expect(@department2.errors.full_messages).to eq(['Name has already been taken'])
-      end
-    end
-    context 'when two organizations has same depertments' do
-      before do
-        @department1 = Department.create(name: 'TAD', organization_id: organization1.id)
-        @department2 = Department.create(name: 'TAD', organization_id: organization2.id)
-      end
-      it 'should validate data with no errors' do
-        @department1.save
-        @department2.save
-        expect(@department2.errors.full_messages).to be_empty
+      it 'should validate data for having same department in the organization' do
+        @department.save
+        @same_department.save
+        expect(@same_department.errors.full_messages).to eq(['Name has already been taken'])
       end
     end
   end 
