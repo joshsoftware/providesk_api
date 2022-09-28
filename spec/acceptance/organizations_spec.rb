d823dfeaa9d957c0d2f8bb4349d1169fced7179c
Addy-tea-ya-official <aditya.yadav19@pccoepune.org>
2022-09-28 11:34:53 +0530

    Add example invalid organization id for context 422

--- spec/acceptance/organizations_spec.rb
+++ spec/acceptance/organizations_spec.rb
@@ -6,21 +6,14 @@ resource "Organizations" do
     @user = FactoryBot.create(:user)
     @department = Department.find(@user.department_id)
     @organization = Organization.find(@user.organization_id)
+    payload = { user_id: @user.id, name: @user.name, email: @user.email, google_user_id: 1 }
+    token = JsonWebToken.encode(payload)
+    header "Accept", "application/vnd.providesk; version=1"
+    header "Authorization", token
   end
   get "/organizations/:id/departments" do
     context '200' do
       let(:id) { @organization.id }
-      before do         
-        payload = { user_id: @user.id,
-          name: @user.name,
-          email: @user.email,
-          google_user_id: 1
-        }
-        token = JsonWebToken.encode(payload)
-        header "Accept", "application/vnd.providesk; version=1"
-        header "Authorization", token
-      end
-      
       example "Listing departments for organization with valid user" do
         expected_response = {
           data:{
@@ -42,14 +35,6 @@ resource "Organizations" do
       let(:id) { @organization1.id }
       before do 
         @organization1 = FactoryBot.create(:organization, name: 'Josh3', domain: ["josh3.com"])
-        payload = { user_id: @user.id,
-          name: @user.name,
-          email: @user.email,
-          google_user_id: 2
-        }
-        token = JsonWebToken.encode(payload)
-        header "Accept", "application/vnd.providesk; version=1"
-        header "Authorization", token
       end
       
       example "User does not have access rights to the content" do
@@ -58,6 +43,17 @@ resource "Organizations" do
         }.to_json
         do_request()
         expect(status).to eq 403
+        response_body.should eq(expected_response)
+      end
+    end
+    context '422' do
+      let(:id) { 0 }
+      example "Pass invalid organization id which does not exist in database" do
+        expected_response = {
+          message: I18n.t('organization.error.invalid_organization_id')
+        }.to_json
+        do_request()
+        expect(status).to eq 422
         response_body.should eq(expected_response)
       end
     end
