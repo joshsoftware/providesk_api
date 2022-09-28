a63eab458bc2fc9cf3b1fbc5455b1ba2d664d282
Addy-tea-ya-official <aditya.yadav19@pccoepune.org>
2022-09-28 18:04:11 +0530

    Add examples for users action in Departments controller

--- spec/acceptance/v1/departments_spec.rb
+++ spec/acceptance/v1/departments_spec.rb
@@ -5,21 +5,12 @@ require 'rspec_api_documentation/dsl'
 
 resource 'Departments' do
   before do
-<<<<<<< HEAD
     @user = FactoryBot.create(:user)
     @org = @user.organization
     @department = FactoryBot.create(:department, name: 'TAD', organization_id: @org.id)
     @some_other_organization = FactoryBot.create(:organization, name: "SomeOther", domain: "someother.com")
     @some_other_department = FactoryBot.create(:department, name: 'TAD', organization_id: @some_other_organization.id)
     payload = { user_id: @user.id, name: @user.name, email: @user.email, google_user_id: 1 }
-=======
-    user = FactoryBot.create(:user)
-    @org = user.organization
-    @department = FactoryBot.create(:department, name: 'TAD', organization_id: @org.id)
-    @some_other_organization = FactoryBot.create(:organization, name: "SomeOther", domain: "someother.com")
-    @some_other_department = FactoryBot.create(:department, name: 'TAD', organization_id: @some_other_organization.id)
-    payload = { user_id: user.id, name: user.name, email: user.email, google_user_id: 1 }
->>>>>>> ce959cea4b92b247d95102c38114193dee2906f1
     token = JsonWebToken.encode(payload)
     header 'Accept', 'application/vnd.providesk; version=1'
     header 'Authorization', token
@@ -59,10 +50,10 @@ resource 'Departments' do
       end
     end
   end
-<<<<<<< HEAD
-  get 'departments/:department_id/users' do
+
+  get 'departments/:id/users' do
     context '200' do
-      let!(:department_id){ @user.department_id }
+      let!(:id){ @user.department_id }
       before do
         @category = FactoryBot.create(:category, name: 'TAD1', priority:1, department_id: @department.id)
       end
@@ -74,8 +65,42 @@ resource 'Departments' do
               {
                 id: @user.id,
                 name: @user.name
-=======
-
+              }
+            ]
+          }
+        }.to_json
+        do_request()
+        response_data = JSON.parse(response_body)
+        expect(response_status).to eq(200)
+        response_body.should eq(expected_response)
+      end
+    end
+    context '422' do
+      let!(:id){ 0 }
+      example 'Pass invalid department id which does not exist on database' do
+        expected_response = {
+          message: I18n.t('department.error.invalid_department_id')
+        }.to_json
+        do_request()
+        response_data = JSON.parse(response_body)
+        expect(response_status).to eq(422)
+        response_body.should eq(expected_response)
+      end
+    end
+    context '403' do
+      let!(:id){ @some_other_department.id }
+      example 'Pass invalid department id of organization to which user is not registered' do
+        expected_response = {
+          message: I18n.t('organization.error.unauthorized_user')
+        }.to_json
+        do_request()
+        response_data = JSON.parse(response_body)
+        expect(response_status).to eq(403)
+        response_body.should eq(expected_response)
+      end
+    end
+  end
+  
   get 'departments/:id/categories' do
     context '200' do
       let!(:id){ @department.id }
@@ -90,7 +115,6 @@ resource 'Departments' do
               {
                 id: @category.id,
                 name: @category.name
->>>>>>> ce959cea4b92b247d95102c38114193dee2906f1
               }
             ]
           }
@@ -103,7 +127,6 @@ resource 'Departments' do
     end
     context '422' do
       let!(:id){ 0 }
-
       example 'Pass invalid department id which does not exist on database' do
         expected_response = {
           message: I18n.t('department.error.invalid_department_id')
@@ -115,15 +138,8 @@ resource 'Departments' do
       end
     end
     context '403' do
-<<<<<<< HEAD
-      let!(:department_id){ @some_other_department.id }
-
-      example 'Pass invalid department id of organization to which user is not registered' do
-=======
       let!(:id){ @some_other_department.id }
-
       example 'Pass invalid department id of wrong organization to which user is not registered' do
->>>>>>> ce959cea4b92b247d95102c38114193dee2906f1
         expected_response = {
           message: I18n.t('organization.error.unauthorized_user')
         }.to_json
