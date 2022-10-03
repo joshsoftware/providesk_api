91336331cd43ecac991b944b004b464556a8157f
Pratham Goel <pratham.goel16@gmail.com>
2022-10-03 15:29:55 +0530

    Added updated to routes. It will update a ticket to get reopen

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   api_version(:module => "Api::V1", :header => {
     name: "Accept", :value => "application/vnd.providesk; version=1"}) do
-      resources :tickets, only: [:create]
+      resources :tickets, only: [:create, :update]
       resources :sessions, only: :create
       resources :categories, only: :create
       resources :departments, only: :create
