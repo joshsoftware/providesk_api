210bcafb247baaa676bf72fda02946654849f800
Nandini Jhanwar <nandinijhanwar67@gmail.com>
2022-10-19 16:09:54 +0530

    Remove show_departments API from organization controller accidently merged while resolving conflicts

--- app/controllers/api/v1/organizations_controller.rb
+++ app/controllers/api/v1/organizations_controller.rb
@@ -17,32 +17,6 @@ module Api::V1
       end
     end
 
-    def show_departments
-      if(current_user.organization_id.eql?(params[:id].to_i))
-        organization = Organization.find(params[:id])
-        departments = Department.where(organization_id: organization.id)
-        departments_list = []
-        departments.each do |department|
-          departments_list.push(
-            {
-              name: department.name,
-              id: department.id
-            }
-          )
-        end
-        render json:{
-          data: {
-            total: departments_list.length,
-            departments: departments_list
-          }
-        }, status: 200
-      else
-        render json:{
-          message: "User not registered to organization"
-        }, status: 403
-      end
-    end  
-
     def departments
       result = Organizations::V1::Departments.new(params, current_user).call
       if result["status"]
