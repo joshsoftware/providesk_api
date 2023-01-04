3a468360c64b7e0ee625d0cc7f50042a957d1729
Nandini Jhanwar <nandinijhanwar67@gmail.com>
2023-01-04 16:22:34 +0530

    Show category id's of user which he belongs to in response of user listing

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,11 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :department_id, :role
+  attributes :id, :name, :department_id, :role, :category_id
 
   def role
     object.role.name
+  end
+
+  def category_id
+    object.user_categories.pluck(:category_id)
   end
 end
