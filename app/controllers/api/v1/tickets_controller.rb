f49bcb4a5a4674ed73988085f628df68ba335e92
Sakshi_Mokashi <117644314+Smokashi23@users.noreply.github.com>
2024-09-24 17:22:55 +0530

    {Staging] Fix/upload s3 bucket (#116)
    * Implemented the presigned URL from backend
    
    * Fixed indentation
    
    * Added service for aws bucket
    
    * fix: indentation
    
    * Added error handling
    
    * Removed space
    
    * Error handling through controller
    
    * Added space
    
    * modified the code

--- app/controllers/api/v1/tickets_controller.rb
+++ app/controllers/api/v1/tickets_controller.rb
@@ -93,6 +93,17 @@ module Api::V1
       end
     end
 
+    def create_presigned_url
+      bucket_name = Rails.application.credentials[:aws][:bucket_name] 
+      object_key = params[:object_key] 
+
+      presigned_url = S3Service.new.get_presigned_url(bucket_name, object_key)
+
+      render json: { url: presigned_url }
+    rescue => e 
+      render json: { message: I18n.t('tickets.error.presigned url')}, status: :unprocessable_entity    
+    end
+
     private
 
     def update_params
---
+++ app/services/s3_service.rb
@@ -0,0 +1,20 @@
+require "aws-sdk-s3"
+require "net/http"
+
+class S3Service
+  def initialize
+    @s3_resource = Aws::S3::Resource.new(
+      region: Rails.application.credentials[:aws][:region]
+      access_key_id: Rails.application.credentials[:aws][:access_key_id]
+      secret_access_key: Rails.application.credentials[:aws][:secret_access_key]
+    )
+  end
+
+  def get_presigned_url(bucket, object_key)
+    bucket = @s3_resource.bucket(bucket)
+    obj = bucket.object(object_key)
+    url = obj.presigned_url(:put)
+    URI(url) 
+  end
+end
+
