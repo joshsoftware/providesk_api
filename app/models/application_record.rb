b1a75230e6588c80f2f77ab45d199b9addfb7eef
Sakshi_Mokashi <117644314+Smokashi23@users.noreply.github.com>
2024-09-24 17:25:08 +0530

    Commented the function of change_In_case (#117)

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,11 +1,11 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
-  def change_case_name
-    if self.name.length <= 3
-      self.name.upcase!
-    else
-      self.name.capitalize!
-    end
-  end
+  # def change_case_name
+  #   if self.name.length <= 3
+  #     self.name.upcase!
+  #   else
+  #     self.name.capitalize!
+  #   end
+  # end
 end
