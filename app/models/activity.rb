67030d06052d541f3ddecacfadd77ede4ac9fa44
Sakshi_Mokashi <117644314+Smokashi23@users.noreply.github.com>
2025-06-04 17:14:47 +0530

    -fixed update status flow (#137)

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -4,8 +4,6 @@ class Activity < ApplicationRecord
   validates :assigned_from, presence: true
   validates :assigned_to, presence: true
   validates :description, presence: true
-  validates :current_ticket_status, inclusion: { in: ["assigned", "inprogress", "for_approval", "closed", "rejected", "on_hold"] }
-  validates :previous_ticket_status, inclusion: { in: ["assigned", "inprogress", "for_approval", "closed", "rejected", "on_hold"] }
 
   enum current_ticket_status: Ticket.statuses, _prefix: :current_status
   enum previous_ticket_status: Ticket.statuses, _prefix: :previous_status
